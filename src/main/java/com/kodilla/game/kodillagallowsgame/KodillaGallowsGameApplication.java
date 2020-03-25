0f5d35bd27d8c6c1492268c6b4a92e2736749469
Ready2112 <kzak.air@gmail.com>
2020-03-25 20:57:24 +0100

    test rep

--- src/main/java/com/kodilla/game/kodillagallowsgame/KodillaGallowsGameApplication.java
+++ src/main/java/com/kodilla/game/kodillagallowsgame/KodillaGallowsGameApplication.java
@@ -8,6 +8,7 @@ public class KodillaGallowsGameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaGallowsGameApplication.class, args);
+        System.out.println("Hello");
     }
 
 }
