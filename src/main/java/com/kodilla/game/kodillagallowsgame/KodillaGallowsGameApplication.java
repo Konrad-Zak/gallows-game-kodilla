58f86f35ff2db9b16657baa2e1c0005586435942
unknown <kzak.air@gmail.com>
2020-03-25 20:39:00 +0100

    inicialized project

---
+++ src/main/java/com/kodilla/game/kodillagallowsgame/KodillaGallowsGameApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.game.kodillagallowsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGallowsGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaGallowsGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/game/kodillagallowsgame/KodillaGallowsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.game.kodillagallowsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaGallowsGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
